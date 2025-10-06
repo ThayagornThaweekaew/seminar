1de3509da0088524f382a63134bcbac6ebe07ef2
Somwan <somwan2547@gmail.com>
2025-10-06 23:07:25 +0700

    Change app host to localhost for local development

--- app.py
+++ app.py
@@ -426,5 +426,5 @@ def delete_plan(pid):
 # ================== RUN ==================
 if __name__ == "__main__":
     # อย่าวาง route/ตั้งค่าเพิ่มเติมไว้ใต้บรรทัดนี้
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
 # ================== APIs for Plan Management ==================
